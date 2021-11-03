acce50ec99deb32cfb15481b3492cf1083146f21
Tim Simms <tsimms@gmail.com>
2021-11-03 10:34:55 -0700

    upload of custom printf function

---
+++ _printf.c
@@ -0,0 +1,79 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * specifiercheck - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
+ */
+int (*specifiercheck(const char *format))(va_list)
+{
+	unsigned int i;
+	specifier p[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_int},
+		{NULL, NULL}
+	};
+
+	for (i = 0; p[i].spec != NULL; i++)
+	{
+		if (*(p[i].spec) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, length = 0;
+	va_list ap;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(ap, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			length++;
+		}
+		if (!format[i])
+			return (length);
+	if (format[i + 1] == '%')
+	{
+		_putchar('%');
+		i += 2;
+	}
+/* add else statement to finish off if 2 % */
+	else
+	{
+		f = specifiercheck(&format[i + 1]);
+	if (f == NULL)
+	{
+	_putchar(format[i]);
+	_putchar(format[i + 1]);
+	i += 2;
+	}
+	else
+	{
+	f(ap);
+	i += 2;
+	}
+	}
+	}
+va_end(ap);
+return (length);
+}
